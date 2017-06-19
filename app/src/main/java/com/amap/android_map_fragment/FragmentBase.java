9ef41c274188270284fee07d67d3269ea28efa48
my94493 <my94493@autonavi.com>
2017-06-19 15:03:08 +0800

    修改地图包

--- app/src/main/java/com/amap/android_map_fragment/FragmentBase.java
+++ app/src/main/java/com/amap/android_map_fragment/FragmentBase.java
@@ -3,11 +3,9 @@ package com.amap.android_map_fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
